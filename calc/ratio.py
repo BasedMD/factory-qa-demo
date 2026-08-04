3456dd44497605041f9af05e2ab3cf5b749286e1
Based Medical <motomusashi1645@gmail.com>
2026-08-04 14:47:42 -0600

    Revert "Fix aspect_ratio: reduce by gcd (#6)"
    This reverts commit 971d15d19d7107bc9a0a483cd3713bcf87ad8590.

--- calc/ratio.py
+++ calc/ratio.py
@@ -1,7 +1,3 @@
-from math import gcd
-
-
 def aspect_ratio(width, height):
     """Reduced integer aspect ratio as a (w, h) tuple, e.g. 1920x1080 -> (16, 9)."""
-    g = gcd(width, height)
-    return (width // g, height // g)
+    return (width, height)
